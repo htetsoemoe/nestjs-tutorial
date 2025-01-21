9d61f4cd9fb98949ab94e278715819995718cdbe
htetsoemoe <soemoehtet.mdy@gmail.com>
2025-01-21 14:12:39 +0630

    literal type and Nullish Coalescing Operator (??)

--- lesson02/src/users/users.controller.ts
+++ lesson02/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
@@ -10,9 +10,16 @@ export class UsersController {
         DELETE /users/:id
     */
 
-    @Get() // GET /users
-    findAll() {
+    @Get() // GET /users or /users?role=value
+    findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') { // role?: literal type
+
+        // Nullish Coalescing Operator (??):
+        // The ?? operator assigns a default value ('NO ROLE') only if role is null or undefined.
+        // This is preferred as it avoids overwriting valid falsy values (e.g., '' or false).
+        const defaultRole = role ?? 'NO ROLE'
+
         return [
+            {role: defaultRole},
             { id: 1, name: 'John Doe', email: 'john@code.com' },
             { id: 2, name: 'Thiri Ko Ko', email: 'thiriko@code.com' },
         ];

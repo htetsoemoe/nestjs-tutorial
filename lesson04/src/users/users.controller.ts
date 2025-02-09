fb3d48d3e44bccf4ca5c9eb379f375a4962c9c14
htetsoemoe <soemoehtet.mdy@gmail.com>
2025-02-09 12:48:44 +0630

    lesson04: nestjs pipes

---
+++ lesson04/src/users/users.controller.ts
@@ -0,0 +1,41 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, ParseIntPipe } from '@nestjs/common';
+import {UsersService} from './users.service'
+
+@Controller('users')
+export class UsersController {
+    // Using Constructor Dependency Injection
+    constructor(private readonly usersService: UsersService) {}
+
+    @Get() // GET /users or /users?role=value
+    findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') { // role?: literal type
+        return this.usersService.findAll(role)
+    }
+
+    @Get(':id') // GET /users/:id
+    findOne(@Param('id', ParseIntPipe) id: number) {
+        return this.usersService.findOne(id)
+    }
+
+    @Post() // POST /users
+    create(@Body() user: {
+        name: string,
+        email: string,
+        role: 'INTERN' | 'ENGINEER' | 'ADMIN' 
+    }) {
+        return this.usersService.create(user)
+    }
+
+    @Patch(':id') // PATCH /users/:id
+    update(@Param('id', ParseIntPipe) id: number, @Body() userUpdate: {
+        name?: string,
+        email?: string,
+        role?: 'INTERN' | 'ENGINEER' | 'ADMIN'
+    }) {
+        return this.usersService.update(id, userUpdate)
+    }
+
+    @Delete(':id') // DELETE /users/:id
+    delete(@Param('id', ParseIntPipe) id: number) {
+        return this.usersService.delete(id)
+    }
+}
